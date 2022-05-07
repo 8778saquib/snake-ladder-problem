6439f07f567d01fda8b16b259bb463b48afe86a8
8778saquib <saquib.nawaz009@gmail.com>
2022-05-07 20:05:30 +0530

    game start

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace snake_ladder_problem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake ladder problem");
+            snakeladder obj = new snakeladder();
+            obj.Start();
+          
         }
     }
 }
---
+++ snakeladder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace snake_ladder_problem
+{
+    public class snakeladder
+    {
+        int initialposition = 0;
+        
+        public void Start()
+        {
+            Console.WriteLine("The initial position of player {0}", initialposition);
+
+        }
+
+    }
+
+}
